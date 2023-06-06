9958918fc78801c467fd0eff019d94c2f9dc522c
mohankumar6789 <112926576+mohankumar6789@users.noreply.github.com>
2023-06-06 20:49:57 +0530

    Three Js code changes
    Three Js code committed changes

---
+++ script/loader.js
@@ -0,0 +1,49 @@
+import { DefaultLoadingManager } from './LoadingManager.js';
+
+class Loader {
+    constructor(manager) {
+        this.manager = (manager !== undefined) ? manager : DefaultLoadingManager;
+        this.crossOrigin = 'anonymous';
+        this.withCredentials = false;
+        this.path = '';
+        this.resourcePath = '';
+        this.requestHeader = {};
+    }
+
+    load() {}
+    loadAsync(url, onProgress) {
+        const scope = this;
+        return new Promise(function (resolve, reject) {
+            scope.load(url, resolve, onProgress, reject);
+        });
+    }
+
+    parse() {}
+
+    setCrossOrigin(crossOrigin) {
+        this.crossOrigin = crossOrigin;
+        return this;
+    }
+
+    setWithCredentials(value) {
+        this.withCredentials = value;
+        return this;
+    }
+
+    setPath(path) {
+        this.path = path;
+        return this;
+    }
+
+    setResourcePath(resourcePath) {
+        this.resourcePath = resourcePath;
+        return this;
+    }
+
+    setRequestHeader(requestHeader) {
+        this.requestHeader = requestHeader;
+        return this;
+    }
+}
+
+export { Loader };
